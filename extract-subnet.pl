192df39ee874587feff3f3c3422c38c7061caa6e
Bob Rogers <rogers@rgrjr.dyndns.org>
2003-07-15 11:26:34 +0000

    extract-subnet.pl:  Added 'use strict' and -w.
    git-svn-id: https://rgrjr.dyndns.org/svn/scripts/trunk@16 94bb4ab6-3451-0410-8010-53dd7b7d8b43

--- extract-subnet.pl
+++ extract-subnet.pl
@@ -1,26 +1,31 @@
-#!/usr/local/bin/perl
+#!/usr/bin/perl -w
 #
 #    Given an interface, extract and print the network address in CIDR format,
-# e.g. "192.168.1.0/24".  If no interface is specified, the first one reported
-# by ifconfig is used.  This is pretty simple; most of the work is involved in
+# e.g. "192.168.1.0/24".  This is pretty simple; most of the work is involved in
 # turning the address and mask into subnet/bits format.
 #
-#    Modification history:
-#
-# created.  -- rgr, 4-Oct-01.
+# [created.  -- rgr, 4-Oct-01.]
 #
 
-open(IN, "ifconfig $ARGV[0] |") || die;
+use strict;
+
+my $iface = shift(@ARGV)
+    || die("$0:  Must give an interface name, e.g. 'eth0', ",
+	   "on the command line.\n");
+
+open(IN, "ifconfig $iface |") || die;
+my $line;
+my @network;
+my $n_bits = 0;
 while (defined($line = <IN>)) {
     if ($line =~ /inet addr: *([\d.]+) .* Mask: *([\d.]+)/) {
-	$addr = $1; $mask = $2;
-	@addr_octets = split(/\./, $addr);
-	@mask_octets = split(/\./, $mask);
-	$n_bits = 0;
-	for ($i = 0; $i < 4; $i++) {
-	    $mask_bits = 0+$mask_octets[$i];
+	my $addr = $1; my $mask = $2;
+	my @addr_octets = split(/\./, $addr);
+	my @mask_octets = split(/\./, $mask);
+	for my $i (0..3) {
+	    my $mask_bits = 0+$mask_octets[$i];
 	    $network[$i] = $addr_octets[$i] & $mask_bits;
-	    # print "$network[$i] = $addr_octets[$i] & $mask_bits;\n";
+	    # count the ones in this mask octet.
 	    while ($mask_bits) {
 		$n_bits++ if $mask_bits & 1;
 		$mask_bits >>= 1;
@@ -29,6 +34,6 @@ while (defined($line = <IN>)) {
 	last;
     }
 }
-die "Couldn't find address for '$ARGV[0]'.\nDied "
+die "Couldn't find address for '$iface'.\n"
     unless @network;
 print (join('.', @network), "/$n_bits\n");
