5d000b547e1673fe403276c51e99911f69800503
Bob Rogers <rogers@rgrjr.dyndns.org>
2004-06-05 16:53:23 +0000

    * skip the site.css "page." * minor formatting changes, trying to make the tables less claustrophobic.
    git-svn-id: https://rgrjr.dyndns.org/svn/scripts/trunk@59 94bb4ab6-3451-0410-8010-53dd7b7d8b43

--- make-popular-pages.pl
+++ make-popular-pages.pl
@@ -38,6 +38,8 @@ my $n = 0;
 my $line;
 while (chomp($line = <LOG>), $line) {
     my ($page, $hits) = split("\t", $line);
+    next
+	if $page eq '/site.css';
     $n++;
     # [this is my definition of "popular."  -- rgr, 14-Feb-03.]
     $n += 10
@@ -46,9 +48,9 @@ while (chomp($line = <LOG>), $line) {
 	&& $page !~ /robots.txt$/) {
 	if ($html_format_p) {
 	    $pages .= join("\n    ",
-			   "  <tr> <td>$n</td>",
-			   "<td> <a href='$page'> <tt>$page</tt></a></td>",
-			   "<td halign=right> $hits</td>\n  </tr>\n");
+			   "  <tr> <td align='right'>$n</td>",
+			   "<td> <a href='$page'><tt>$page</tt></a></td>",
+			   "<td align='right'> $hits</td>\n  </tr>\n");
 	}
 	else {
 	    print join("\t", $hits, $page), "\n";
@@ -62,7 +64,7 @@ print <<EOF1
 <h3>Most popular pages</h3>
 
 <blockquote>
-<table>
+<table cellpadding="2">
   <tr> <th>Rank</th> <th>Page URL</th> <th>Hits</th></tr>
 $pages
 </table>
