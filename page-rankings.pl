5361add9d9e2341aab6f390d17a7012b94093f07
Bob Rogers <rogers@rgrjr.dyndns.org>
2005-02-20 00:37:27 +0000

    Added --verbose and --top options, added some %excluded_pages entries (all on 25-Dec-04).
    git-svn-id: https://rgrjr.dyndns.org/svn/scripts/trunk@81 94bb4ab6-3451-0410-8010-53dd7b7d8b43

--- page-rankings.pl
+++ page-rankings.pl
@@ -2,11 +2,9 @@
 #
 #    Generates a page of the most popular pages/directories on the site.
 #
-#    Modification history:
+# [created.  -- rgr, 14-Feb-03.]
 #
-# created.  -- rgr, 14-Feb-03.
-# --[no]squid and --[no]html switches.  -- rgr, 2-Apr-03.
-#
+# $Id$
 
 use strict;
 use Getopt::Long;
@@ -15,14 +13,22 @@ my $warn = 'make-popular-pages.pl';
 
 my $squid_format_p = 1;
 my $html_format_p = 1;
+my $verbose_p = 0;
 my $robots_p = 0;
 # [this is my definition of "popular."  -- rgr, 14-Feb-03.]
 my %excluded_pages = ('/site.css' => 1,
 		      '/random/doubleclick.png' => 1,
+		      '/linux/striped-blue.png' => 1,
+		      '/linux/striped-green.png' => 1,
+		      '/linux/striped-brown.png' => 1,
+		      '/linux/striped-orange.png' => 1,
 		      '/bob/resume.html' => 'last',
 		      '/robots.txt' => 1);
+my $n_top_hits = 10;
 
-GetOptions(# 'help' => \$help, 'man' => \$man, 'verbose+' => \$verbose_p,
+GetOptions(# 'help' => \$help, 'man' => \$man, 
+	   'verbose+' => \$verbose_p,
+	   'top=i' => \$n_top_hits,
 	   'squid!' => \$squid_format_p,
 	   'html!' => \$html_format_p,
 	   'robots!' => \$robots_p)
@@ -32,21 +38,25 @@ GetOptions(# 'help' => \$help, 'man' => \$man, 'verbose+' => \$verbose_p,
 my $log_name = shift(@ARGV);
 die "$warn:  '$log_name' is not readable:  $!\n"
     unless -r $log_name;
-open(LOG,
-     "fgrep -v 192.168.57. $log_name "
-     .($squid_format_p ? "| fgrep -v 66.31.124.111 | squid2std.pl " : '')
-     ."| page-hits.pl -page-totals |")
+my $log_pipe
+    = join(' | ', 
+	   "fgrep -v 192.168.57. $log_name ",
+	   "fgrep -v 24.34.108.24",
+	   ($squid_format_p ? "fgrep -v 66.31.124.111 | squid2std.pl" : ()),
+	   "page-hits.pl -page-totals");
+warn "[got log pipe '$log_pipe']\n"
+    if $verbose_p;
+open(LOG, "$log_pipe |")
     or die;
 
 my $pages = '';		# contains popular page hits.
 my $n = 0;
 my $line;
-while ($n < 10 && ($line = <LOG>)) {
+while ($n < $n_top_hits && ($line = <LOG>)) {
     chomp($line);
     my ($page, $hits) = split("\t", $line);
     my $excluded_p = $excluded_pages{$page} || 0;
-    last
-	if $excluded_p eq 'last';
+    # last if $excluded_p eq 'last';
     next
 	if $excluded_p;
     $n++;
