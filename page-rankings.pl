2c097e8cd3aa2c0b688c8f34b1d145fb565f9e36
Bob Rogers <rogers@rgrjr.dyndns.org>
2005-03-05 02:17:17 +0000

    + Rewritten to emit a table summarizing each page's hits over      multiple time periods, with rows for 'other' and 'total' hits.    + Rename from make-popular-pages.pl to page-rankings.pl.    + --nosquid is now the default.
    git-svn-id: https://rgrjr.dyndns.org/svn/scripts/trunk@83 94bb4ab6-3451-0410-8010-53dd7b7d8b43

--- page-rankings.pl
+++ page-rankings.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 #
-#    Generates a page of the most popular pages/directories on the site.
+#    Given a series of log files, generates a period-by-period table of hits.
 #
 # [created.  -- rgr, 14-Feb-03.]
 #
@@ -9,9 +9,7 @@
 use strict;
 use Getopt::Long;
 
-my $warn = 'make-popular-pages.pl';
-
-my $squid_format_p = 1;
+my $squid_format_p = 0;
 my $html_format_p = 1;
 my $verbose_p = 0;
 my $robots_p = 0;
@@ -32,55 +30,81 @@ GetOptions(# 'help' => \$help, 'man' => \$man,
 	   'squid!' => \$squid_format_p,
 	   'html!' => \$html_format_p,
 	   'robots!' => \$robots_p)
-    or die("usage:  $warn [ --[no]squid ] [ --[no]html ] ",
-	   "[ --[no]robots ] log_file_name\n");
+    or die("usage:  $0 [ --[no]squid ] [ --[no]html ] ",
+	   "[ --[no]robots ] log_file_name ...\n");
 
-my $log_name = shift(@ARGV);
-die "$warn:  '$log_name' is not readable:  $!\n"
-    unless -r $log_name;
-my $log_pipe
-    = join(' | ', 
-	   "fgrep -v 192.168.57. $log_name ",
-	   "fgrep -v 24.34.108.24",
-	   ($squid_format_p ? "fgrep -v 66.31.124.111 | squid2std.pl" : ()),
-	   "page-hits.pl -page-totals");
-warn "[got log pipe '$log_pipe']\n"
-    if $verbose_p;
-open(LOG, "$log_pipe |")
-    or die;
+# the first file is the one that generates the rankings.
+my $log_index = 0;
+my %page_entries;
+my @most_popular;
+my $other_entry = [ 'Other hits:' ];
+my $total_entry = [ 'Monthly total hits:' ];
+for my $log_name (@ARGV) {
+    die "$0:  '$log_name' is not readable:  $!\n"
+	unless -r $log_name;
+    $log_index++;
+    my $log_pipe
+	= join(' | ', 
+	       "fgrep -v 192.168.57. $log_name ",
+	       "fgrep -v 24.34.108.24",
+	       ($squid_format_p ? "fgrep -v 66.31.124.111 | squid2std.pl" : ()),
+	       "page-hits.pl -page-totals -no-summary");
+    warn "[got log pipe '$log_pipe']\n"
+	if $verbose_p;
+    open(LOG, "$log_pipe |")
+	or die;
 
-my $pages = '';		# contains popular page hits.
-my $n = 0;
-my $line;
-while ($n < $n_top_hits && ($line = <LOG>)) {
-    chomp($line);
-    my ($page, $hits) = split("\t", $line);
-    my $excluded_p = $excluded_pages{$page} || 0;
-    # last if $excluded_p eq 'last';
-    next
-	if $excluded_p;
-    $n++;
+    my $line;
+    my $rank = 0;
+    my $total_hits = 0;
+    my $other_hits = 0;
+    while ($line = <LOG>) {
+	last
+	    if $line =~ /^Totals:/;
+	chomp($line);
+	my ($page, $hits) = split("\t", $line);
+	next
+	    unless $page;
+	my $excluded_p = $excluded_pages{$page} || 0;
+	next
+	    # Don't even count these in "other hits."
+	    if $excluded_p;
+	$rank++;
+	my $entry = $page_entries{$page};
+	$entry = $page_entries{$page} = [ $page ]
+	    unless $entry;
+	$entry->[2*$log_index-1] = $rank;
+	$entry->[2*$log_index] = $hits;
+	$total_hits += $hits;
+	$other_hits += $hits
+	    unless defined($entry->[1]) && $entry->[1] <= $n_top_hits;
+	push(@most_popular, $entry)
+	    if $log_index == 1;
+    }
+    # make 'other' and 'totals' entries for this file.
+    $other_entry->[2*$log_index-1] = '&nbsp;';
+    $other_entry->[2*$log_index] = $other_hits;
+    $total_entry->[2*$log_index-1] = '&nbsp;';
+    $total_entry->[2*$log_index] = $total_hits;
+    close(LOG);
+}
+
+for my $entry (@most_popular[0..$n_top_hits-1], $other_entry, $total_entry) {
+    my $page = $entry->[0];
     if ($html_format_p) {
-	$pages .= join("\n    ",
-		       "  <tr> <td align='right'>$n</td>",
-		       "<td> <a href='$page'><tt>$page</tt></a></td>",
-		       "<td align='right'> $hits</td>\n  </tr>\n");
+	my $link = ($page =~ /:/
+		    ? $page
+		    : "<a href='$page'><tt>$page</tt></a>");
+	print("  <tr> <td>$link</td>\n");
+	for my $i (1..$log_index) {
+	    my $rank = $entry->[2*$i-1] || '--';
+	    my $hits = $entry->[2*$i] || '&nbsp;';
+	    print("    <td align='right'>$rank</td>\n",
+		  "    <td align='right'>$hits</td>\n");
+	}
+	print("  </tr>\n");
     }
     else {
-	print join("\t", $hits, $page), "\n";
+	print(join("\t", map { $_ || ''; } @$entry), "\n");
     }
 }
-close(LOG);
-exit(0)
-    if ! $html_format_p;
-print <<EOF1
-<h3>Most popular pages</h3>
-
-<blockquote>
-<table cellpadding="2">
-  <tr> <th>Rank</th> <th>Page URL</th> <th>Hits</th></tr>
-$pages
-</table>
-</blockquote>
-EOF1
-    ;
