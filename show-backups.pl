44b1a8ff34b6a71969825e6a3eea17f9f954a806
Bob Rogers <rogers@rgrjr.dyndns.org>
2005-11-04 20:04:40 +0000

    + Sort on base name, and allow for the possibility that there may be      multiple dumps for a given prefix & date.  This could be either a      single backup split across multiple files, or perhaps somebody was      careless enough to perform backups at two different levels on the      same day.
    git-svn-id: https://rgrjr.dyndns.org/svn/scripts/trunk@104 94bb4ab6-3451-0410-8010-53dd7b7d8b43

--- show-backups.pl
+++ show-backups.pl
@@ -8,7 +8,6 @@
 
 use strict;
 use Getopt::Long;
-# use Data::Dumper;
 
 my $host_name = `hostname`;
 chomp($host_name);
@@ -47,31 +46,41 @@ while (<IN>) {
 	my @stat = stat($file);
 	my $size = $stat[7];
 	$file =~ s@(/.*/)(.*)$@$2 [$host_name:$1]@;
+	my $base_name = $2;
 	# [sprintf can't handle huge numbers.  -- rgr, 28-Jun-04.]
 	# my $listing = sprintf('%14i %s', $size, $file);
 	my $listing = (' 'x(14-length($size))).$size.' '.$file;
 	push(@{$prefix_and_date_to_dumps{$pfx}->{$date}},
-	     ["$pfx-$date", $level, $listing]);
+	     ["$pfx-$date", $level, $listing, $base_name]);
     }
 }
 
 # For each prefix, generate output sorted with the most recent at the top, and a
-# '*' marking the current backup files.  (Of course, we only know what is
-# "current" in local terms.)
-my $last_level = 10;
-my $last_pfx_date = '';
-# warn Dumper(\%prefix_and_date_to_dumps);
+# '*' marking each of the current backup files.  (Of course, we only know which
+# files are "current" in local terms.)
 my $n_prefixes = 0;
 for my $pfx (sort(keys(%prefix_and_date_to_dumps))) {
     my $date_to_dumps = $prefix_and_date_to_dumps{$pfx};
     print "\n"
 	if $n_prefixes;
+    my $star_p = 0;
+    my $last_star_level = 10;
+    my $last_pfx_date = '';
     for my $date (sort { $b <=> $a; } keys(%$date_to_dumps)) {
-	for my $entry (@{$date_to_dumps->{$date}}) {
+	my $entries = $date_to_dumps->{$date};
+	# this sorts first by level (in case somebody is careless enough to
+	# perform backups at two different levels on the same day), and then by
+	# file name (for when a single backup is split across multiple files).
+	for my $entry (sort { $a->[1] <=> $b->[1]
+				  || $a->[3] cmp $b->[3]; } @$entries) {
 	    my ($pfx_date, $level, $listing) = @$entry;
-	    substr($listing, 1, 1) = '*', $last_level = $level
-		if ($level < $last_level
-		    || ($level == $last_level && $pfx_date eq $last_pfx_date));
+	    $star_p = ($pfx_date eq $last_pfx_date
+		       # same dump, no change in $star_p.
+		       ? $star_p
+		       # put a star if more comprehensive than the last.
+		       : $level < $last_star_level);
+	    substr($listing, 1, 1) = '*', $last_star_level = $level
+		if $star_p;
 	    print $listing, "\n";
 	    $last_pfx_date = $pfx_date;
 	}
