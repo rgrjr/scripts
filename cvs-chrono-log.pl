767ebdb2c88519f309c74140201f8ac307cb6c5d
Bob Rogers <rogers@rgrjr.dyndns.org>
2006-02-21 01:59:09 +0000

    + (record_file_rev_comment):  Take note of commitid, which allows for      more robust reassembly of commit transations.    + (print_file_rev_comments):  Unify revs with and without commitid,      change output format to use RGR::CVS::FileRevision and take      advantage of extra parsing, fix some more early EOF problems.    + (RGR::CVS::FileRevision):  New class for recording entries.
    git-svn-id: https://rgrjr.dyndns.org/svn/scripts/trunk@111 94bb4ab6-3451-0410-8010-53dd7b7d8b43

--- cvs-chrono-log.pl
+++ cvs-chrono-log.pl
@@ -13,7 +13,8 @@ use Date::Format;
 
 my $date_format_string = '%Y-%m-%d %H:%M';
 my $date_fuzz = 120;		# in seconds.
-my %modifications;
+my %commit_mods;		# arrayrefs keyed on commitid.
+my %comment_mods;		# arrayrefs keyed on comment and date.
 
 sub record_file_rev_comment {
     my ($file_name, $file_rev, $date_etc, $comment) = @_;
@@ -25,24 +26,45 @@ sub record_file_rev_comment {
     }
     else {
 	my $encoded_date = str2time($date, 'UTC');
-	$date_etc =~ s/state: Exp; *//;
-	push(@{$modifications{$comment}{$encoded_date}},
-	     [$file_name, $file_rev, $date_etc]);
+	$date_etc =~ s/; *$//;
+	my $rev = new RGR::CVS::FileRevision
+	    (raw_date => $date, encoded_date => $encoded_date,
+	     comment => $comment,
+	     file_name => $file_name, file_rev => $file_rev,
+	     map { split(/: */, $_, 2); } split(/; +/, $date_etc));
+	my $commit_id = $rev->commitid;
+	if ($commit_id) {
+	    push(@{$commit_mods{$commit_id}}, $rev);
+	}
+	else {
+	    push(@{$comment_mods{$comment}{$encoded_date}}, $rev);
+	}
     }
 }
 
+my $per_entry_fields = [ qw(author commitid) ];
+my $per_file_fields = [ qw(state lines) ];
 sub print_file_rev_comments {
     # Print all revision comments, sorted by date and grouped by comment.
 
-    # examine entries by comment, then by date, combining all that have the
-    # identical comment and nearly the same date.  [perhaps we should also
-    # refuse to merge them if their modified files are not disjoint.  -- rgr,
-    # 29-Aug-05.]
+    # combine file entries that correspond to a single commit.
     my @combined_entries;
-    for my $comment (sort(keys(%modifications))) {
+    for my $commit_id (sort(keys(%commit_mods))) {
+	# All entries with the same commitid perforce belong to the same commit,
+	# to which no entries without a commitid can belong.
+	my $entries = $commit_mods{$commit_id};
+	my $entry = $entries->[0];
+	push(@combined_entries,
+	     [$entry->encoded_date, $entry->comment, @$entries]);
+    }
+    # examine remaining entries by comment, then by date, combining all that
+    # have the identical comment and nearly the same date.  [perhaps we should
+    # also refuse to merge them if their modified files are not disjoint.  --
+    # rgr, 29-Aug-05.]
+    for my $comment (sort(keys(%comment_mods))) {
 	my $last_date;
 	my @entries;
-	for my $date (sort(keys(%{$modifications{$comment}}))) {
+	for my $date (sort(keys(%{$comment_mods{$comment}}))) {
 	    if ($last_date && $date-$last_date > $date_fuzz) {
 		# the current entry probably represents a "cvs commit" event
 		# that is distinct from the previous entry(ies).
@@ -51,7 +73,7 @@ sub print_file_rev_comments {
 		undef($last_date);
 	    }
 	    $last_date ||= $date;
-	    push(@entries, @{$modifications{$comment}{$date}});
+	    push(@entries, @{$comment_mods{$comment}{$date}});
 	}
 	push(@combined_entries, [$last_date, $comment, @entries])
 	    if @entries;
@@ -60,7 +82,8 @@ sub print_file_rev_comments {
     for my $date_entry (sort { -($a->[0] <=> $b->[0]); } @combined_entries) {
 	my $date = time2str($date_format_string, shift(@$date_entry));
 	my $comment = shift(@$date_entry);
-	print "$date:\n";
+	print("$date:\n",
+	      "  ", $date_entry->[0]->join_fields($per_entry_fields), "\n");
 	for my $line (split("\n", $comment)) {
 	    print "  $line\n"
 		if $line;
@@ -69,11 +92,13 @@ sub print_file_rev_comments {
 	# dates can make them come unsorted.
 	my ($n_matches, $n_files) = (0, 0);
 	my ($lines_removed, $lines_added);
-	for my $entry (sort { $a->[0] cmp $b->[0]; } @$date_entry) {
-	    my ($file_name, $file_rev, $date_etc) = @$entry;
-	    print "  => $file_name $file_rev:  $date_etc\n";
+	for my $entry (sort { $a->file_name cmp $b->file_name; } @$date_entry) {
+	    print(join(' ', "  =>", $entry->file_name, $entry->file_rev,
+		       ': ', $entry->join_fields($per_file_fields)),
+		  "\n");
+	    my $lines = $entry->lines;
 	    $lines_added += $1, $lines_removed += $2, $n_matches++
-		if $date_etc =~ /lines: \+(\d+) -(\d+)/;
+		if $lines && $lines =~ /\+(\d+) -(\d+)/;
 	    $n_files++;
 	}
 	print("     Total lines: +$lines_added -$lines_removed", 
@@ -107,12 +132,12 @@ while (defined($line = <>)) {
 	    $date_etc .= "  ".$line;
 	    $line = <>;
 	}
-	while ($line !~ /^---+$|^===+$/) {
+	while ($line && $line !~ /^---+$|^===+$/) {
 	    $comment .= $line;
 	    $line = <>;
 	}
 	record_file_rev_comment($file_name, $file_rev, $date_etc, $comment);
-	$state = ($line =~ /^========/ ? 'none' : 'descriptions');
+	$state = (! $line || $line =~ /^========/ ? 'none' : 'descriptions');
     }
     # $state eq 'headings'
     elsif ($line =~ /^([^:]+):\s*(.*)/) {
@@ -136,3 +161,47 @@ while (defined($line = <>)) {
 warn "[oops; final state is $state.]\n"
     unless $state eq 'none';
 print_file_rev_comments();
+
+package RGR::CVS::FileRevision;
+
+# E.g.: date: 2006-02-20 23:37:32 +0000; author: rogers; state: Exp; lines: +1
+# -3; commitid: 4b443fa52b84567;
+
+# define instance accessors.
+sub BEGIN {
+  no strict 'refs';
+  for my $method (qw(comment raw_date encoded_date file_name file_rev
+		     author state lines commitid)) {
+    my $field = '_' . $method;
+    *$method = sub {
+      my $self = shift;
+      @_ ? ($self->{$field} = shift, $self) : $self->{$field};
+    }
+  }
+}
+
+sub new {
+  my $class = shift;
+
+  my $self = bless({}, $class);
+  while (@_) {
+      my $method = shift;
+      my $argument = shift;
+      $self->$method($argument)
+	  if $self->can($method);
+  }
+  $self;
+}
+
+sub join_fields {
+    my ($self, $fields) = @_;
+
+    join(';  ',
+	 map {
+	     my $name = $_;
+	     my $value = $self->$name;
+	     (defined($value)
+	      ? "$name: $value"
+	      : ());
+	 } @$fields);
+}
