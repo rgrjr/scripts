6022dad7dd94f52e318b668037da8cf59090fcdf
Bob Rogers <rogers@rgrjr.dyndns.org>
2007-12-26 23:45:58 +0000

    + Add /var/log/firewall if it is readable.
    git-svn-id: https://rgrjr.dyndns.org/svn/scripts/trunk@161 94bb4ab6-3451-0410-8010-53dd7b7d8b43

--- daily-status.pl
+++ daily-status.pl
@@ -12,12 +12,16 @@ use warnings;
 # File which keeps track of when we last ran.
 my $status_since = '/root/bin/status-since';
 
+my @log_files = qw(/var/log/messages);
+push(@log_files, '/var/log/firewall')
+    if -r '/var/log/firewall';
+
 chomp(my $yesterday = `date -r $status_since '+%b %d'`);
 # since date gives "Apr 05".
 $yesterday =~ s/ 0/  /;
 system(join(' ',
 	    '/usr/local/bin/check-logs.pl',
 	    ($yesterday ? "-from '$yesterday'" : ()),
-	    '/var/log/messages',
+	    @log_files,
 	    "| mail rogers -s 'Daily log for $yesterday'");
 system("touch $status_since");
