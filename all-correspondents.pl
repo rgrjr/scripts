dc348b1e28a8c1aa9b5a13d6f4581571d7a648bf
Bob Rogers <rogers@rgrjr.dyndns.org>
2021-02-02 12:51:05 -0500

    * all-correspondents.pl:    + Document, and add --help, --man, and --usage options.

--- all-correspondents.pl
+++ all-correspondents.pl
@@ -1,18 +1,31 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 #
 # Extract all recipients from an mbox file.
 #
 # [created.  -- rgr, 28-Sep-03.]
 #
-# $Id$
 
 use strict;
+use warnings;
 
+use Getopt::Long;
+use Pod::Usage;
+
+my $help = 0;
+my $man = 0;
+my $usage = 0;
 my $debug_p = 0;
 # The stuff in the middle (matched by "\S*") is the envelope sender -- we need
 # "\S*" instead of "\S+" because bounces get "<>" as the sender address.  We
 # just want to get to the DOW to help eliminate false hits.
 my $from_line_regexp = '^From \S* +(Sun|Mon|Tue|Wed|Thu|Fri|Sat) ';
+
+GetOptions('help' => \$help, 'man' => \$man, 'usage' => \$usage,
+	   'verbose|v+' => \$debug_p)
+    or pod2usage(2);
+pod2usage(2) if $usage;
+pod2usage(1) if $help;
+pod2usage(-exitstatus => 0, -verbose => 2) if $man;
 
 ### Subroutines.
 
@@ -115,3 +128,64 @@ else {
 for my $address (sort(keys(%addresses))) {
     print "$address\n";
 }
+
+__END__
+
+=head1 NAME
+
+all-correspondents.pl - extract addresses from mbox files
+
+=head1 SYNOPSIS
+
+    all-correspondents.pl [ --help ] [ --man ] [ --usage ] [ --verbose ... ]
+    			  [ mbox-file ... ] > correpondents.text
+
+=head1 DESCRIPTION
+
+Given one or more mbox files on the command line or the standard
+input, extract all unique email addresses in "Sender:", "From:", and
+"Reply-To:"  headers, and spit them out on the standard output.
+
+=head1 OPTIONS
+
+As with all other C<Getopt::Long> scripts, option names can be
+abbreviated to anything long enough to be unambiguous (e.g. C<--line-len>
+or C<--lin> for C<--line-length>), options with arguments can be given as
+two words (e.g. C<--line 100>) or in one word separated by an "="
+(e.g. C<--line=100>), and "-" can be used instead of "--".
+
+=over 4
+
+=item B<--help>
+
+Prints the L<"SYNOPSIS"> and L<"OPTIONS"> sections of this documentation.
+
+=item B<--man>
+
+Prints the full documentation in the Unix `manpage' style.
+
+=item B<--usage>
+
+Prints just the L<"SYNOPSIS"> section of this documentation.
+
+=item B<--verbose>
+
+Prints debugging information if specified.
+
+=back
+
+=head1 BUGS
+
+If you find any, please let me know.
+
+=head1 COPYRIGHT
+
+ Copyright (C) 2003-2021 by Bob Rogers <rogers@rgrjr.dyndns.org>.
+ This script is free software; you may redistribute it and/or modify it
+ under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Bob Rogers C<E<lt>rogers@rgrjr.dyndns.orgE<gt>>
+
+=cut
